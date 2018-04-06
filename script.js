9a44f81a9d3b3815006f6a69b449adb056d95abe
Damian Stepniak <95021613456@st.wns.uph.edu.pl>
2018-04-07 01:06:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿var scopeApp = angular.module('scotchApp', ['ngRoute']);
+scopeApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl: 'pages/contact.html',
+            controller: 'contectController'
+        })
+        .when('/cv', {
+            templateUrl: 'pages/cv.html',
+            controller: 'cvController'
+        });
+});
+
+scopeApp.controller('mainController', function ($scope) {
+    $scope.message = "Witaj na mojej stronie!";
+});
+
+scopeApp.controller('aboutController', function ($scope) {
+    $scope.message = "Tu powiem cos wiecej o mnie";
+});
+
+scopeApp.controller('contectController', function ($scope) {
+    $scope.message = "Skontaktuj sie ze mną! JK this is jus a demo.";
+});
+
+scopeApp.controller('cvController', function ($scope) {
+    $scope.message = "Tu jest moje CV!";
+});
